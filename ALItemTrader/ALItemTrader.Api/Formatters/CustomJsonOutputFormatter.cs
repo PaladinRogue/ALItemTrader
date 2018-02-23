d814fa6612a2c7d517681cd400e995ce3187c8b5
Thomas Ryder <thomas.ryder@gmail.com>
2018-02-23 16:30:00 +0000

    json formatter

---
+++ ALItemTrader/ALItemTrader.Api/Formatters/CustomJsonOutputFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System.Buffers;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+
+namespace ALItemTrader.Api.Formatters
+{
+    public class CustomJsonOutputFormatter : JsonOutputFormatter
+    {
+        public CustomJsonOutputFormatter(JsonSerializerSettings serializerSettings, ArrayPool<char> charPool)
+            : base(serializerSettings, charPool)
+        {
+        }
+    }
+}
